0c810d16dac0654198697c6b5e154440259347e3
Tarcisio Lima <tarcisio_thallys@hotmail.com>
2020-04-17 17:33:26 -0300

    (feat): init mobile.

---
+++ mobile/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {View} from 'react-native';
+
+// import { Container } from './styles';
+import Teste from '~/';
+
+export default function src() {
+  return <View />;
+}
