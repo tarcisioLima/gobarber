1b98309bae0655c9ca71d7c1a368212997b2fd09
Tarcisio Lima <tarcisio_thallys@hotmail.com>
2020-04-17 20:02:26 -0300

    Mobile default configs.

--- mobile/src/Teste.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import {Text} from 'react-native';
-
-export default function src() {
-  return <Text>AEEEE FOI</Text>;
-}
--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {View} from 'react-native';
 
+import Routes from '~/routes';
+
 // import { Container } from './styles';
-import Teste from '~/';
 
 export default function src() {
-  return <View />;
+  return <Routes />;
 }
