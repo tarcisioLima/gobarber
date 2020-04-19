55e97aec01c2def2ca72de56b6a77ef3fa111186
Tarcisio Lima <tarcisio_thallys@hotmail.com>
2020-04-19 14:45:25 -0300

    (feat): mobile persist.

--- mobile/src/pages/SignUp/index.js
+++ mobile/src/pages/SignUp/index.js
@@ -1,9 +1,11 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Image } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
 
 import logo from '~/assets/logo.png';
 
 import Background from '~/components/Background';
+import { signUpRequest } from '~/store/modules/auth/actions';
 
 import {
   Container,
@@ -15,10 +17,19 @@ import {
 } from './styles';
 
 export default function SignUp({ navigation }) {
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.auth.loading);
+
   const emailRef = useRef();
   const passwordRef = useRef();
 
-  function handleSubmit() {}
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit() {
+    dispatch(signUpRequest(name, email, password));
+  }
 
   return (
     <Background>
@@ -35,6 +46,8 @@ export default function SignUp({ navigation }) {
             onSubmitEditing={() => {
               emailRef.current.focus();
             }}
+            value={name}
+            onChangeText={setName}
           />
           <FormInput
             icon="mail-outline"
@@ -47,6 +60,8 @@ export default function SignUp({ navigation }) {
             onSubmitEditing={() => {
               passwordRef.current.focus();
             }}
+            value={email}
+            onChangeText={setEmail}
           />
           <FormInput
             icon="lock-outline"
@@ -55,11 +70,10 @@ export default function SignUp({ navigation }) {
             ref={passwordRef}
             returnKeyType="send"
             onSubmitEditing={handleSubmit}
+            value={password}
+            onChangeText={setPassword}
           />
-          <SubmitButton
-            onPress={() => {
-              handleSubmit;
-            }}>
+          <SubmitButton loading={loading} onPress={handleSubmit}>
             Criar conta
           </SubmitButton>
         </Form>
