031482998584f0810d5697974fdc6e74eee36bf6
Tarcisio Lima <tarcisio_thallys@hotmail.com>
2020-04-20 11:11:08 -0300

    (feat): Logout app.

--- mobile/src/App.js
+++ mobile/src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-//import { useDispatch } from 'react-redux';
-//import { signOut } from '~/store/modules/auth/actions';
-
 import createRouter from '~/routes';
 
 export default function App() {
-  //const dispatch = useDispatch();
-  //dispatch(signOut());
-
   const signed = useSelector((state) => state.auth.signed);
 
   const Routes = createRouter(signed);
